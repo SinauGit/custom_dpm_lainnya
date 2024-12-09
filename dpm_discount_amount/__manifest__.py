de042915f49a437f7490357d2a25ffb32ba78297
SinauGit <newprogrammerjava@gmail.com>
2024-12-09 16:20:09 +0700

    Committing changes before adding new files

---
+++ dpm_discount_amount/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "DPM Discount Amount",
+    "version": "17.0.1.1.0",
+    'summary': '',
+    'author': '',
+    "depends": ["account"],
+    "data": [
+        "security/res_groups.xml",
+        "views/account_move_view.xml",
+        "reports/report_account_invoice.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
