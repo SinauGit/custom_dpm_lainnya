f3bdcd61b8b221b4cf9b23ad6ed5ca16ef090264
SinauGit <newprogrammerjava@gmail.com>
2024-12-11 16:33:28 +0700

    bug discount fixed

--- dpm_discount_amount/models/account_tax.py
+++ dpm_discount_amount/models/account_tax.py
@@ -6,6 +6,9 @@ class AccountTax(models.Model):
 
     def _convert_to_tax_base_line_dict(self, base_line, **kwargs):
         res = super()._convert_to_tax_base_line_dict(base_line, **kwargs)
-        if base_line and base_line.discount_fixed and base_line._name in ["sale.order.line", "account.move.line"]:
+        if (base_line and 
+            hasattr(base_line, 'discount_fixed') and 
+            base_line.discount_fixed and 
+            base_line._name in ["sale.order.line", "account.move.line"]):
             res["discount"] = base_line._get_discount_from_fixed_discount()
         return res
