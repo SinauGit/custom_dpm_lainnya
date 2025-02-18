d9af1c7006252694ee6bc8d372b5d0a3637f7cce
SinauGit <newprogrammerjava@gmail.com>
2025-02-18 19:03:09 +0700

    bisa donggggg fiks bisa otomatis update data uniq code nya

--- uniq_number/models/account_move.py
+++ uniq_number/models/account_move.py
@@ -1,19 +1,25 @@
 from odoo import fields, models, api
 
-
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    # Related fields untuk kedua kode
+    customer_code = fields.Char(related='partner_id.customer_code', store=True)
+    vendor_code = fields.Char(related='partner_id.vendor_code', store=True)
+
+    # Field utama yang dikondisikan
     partner_unique_id = fields.Char(
-        compute='_compute_partner_code',
-        string='Customer Code',
-        store=True,
+        string='Customer/Vendor Code',
+        compute='_compute_partner_unique_id',
+        store=True
     )
-    
-    @api.depends('partner_id', 'move_type')
-    def _compute_partner_code(self):
+
+    @api.depends('move_type', 'customer_code', 'vendor_code')
+    def _compute_partner_unique_id(self):
         for move in self:
             if move.move_type in ('out_invoice', 'out_refund', 'out_receipt'):
-                move.partner_unique_id = move.partner_id.customer_code
+                move.partner_unique_id = move.customer_code
+            elif move.move_type in ('in_invoice', 'in_refund', 'in_receipt'):
+                move.partner_unique_id = move.vendor_code
             else:
-                move.partner_unique_id = move.partner_id.vendor_code
+                move.partner_unique_id = False
